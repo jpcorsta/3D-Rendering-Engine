7bebbff9c24fe22ce5b41c2a2cf054f051fe16a8
jean-paul jeursen <jpcorsta@mail.com>
2026-02-22 20:30:23 +0100

    https://www.youtube.com/watch?v=oHP3QFvNpn4&list=PLaoUW4S6Hk__bLDdXkDNOW7yJ3jQkrdWS&t=15m11s

--- 3D Rendering Engine/Form1.cs
+++ 3D Rendering Engine/Form1.cs
@@ -6,19 +6,40 @@ namespace _3D_Rendering_Engine
 	{
 		float focalLength = 200f;
 
-		List<Vector3> points = new List<Vector3>();
+		List<(Vector3, Vector3, Vector3)> points = new List<(Vector3, Vector3, Vector3)>();
+
+		Vector2 ProjectPoints(Vector3 point)
+		{
+			int ScreenX = (int)(this.Width / 2 + (point.X / point.Z) * focalLength);
+			int ScreenY = (int)(this.Height / 2 - (point.Y / point.Z) * focalLength);
+
+			return new Vector2(ScreenX, ScreenY);
+		}
 
 		public Form1()
 		{
 			InitializeComponent();
+
+			// front face
+			points.Add(new Vector3(-1, -1, 2));
+			points.Add(new Vector3(1, -1, 2));
+			points.Add(new Vector3(-1, 1, 2));
+			points.Add(new Vector3(1, 1, 2));
+
+			// front face
+			points.Add(new Vector3(-1, -1, 4));
+			points.Add(new Vector3(1, -1, 4));
+			points.Add(new Vector3(-1, 1, 4));
+			points.Add(new Vector3(1, 1, 4));
+
+
 		}
 
 		private void Form1_Paint(object sender, PaintEventArgs e)
 		{
-			foreach (var point in points) 
+			foreach (var triangle in points) 
 			{
-				int ScreenX = (int)(this.Width / 2 + (point.X / point.Z) * focalLength);
-				int ScreenY = (int)(this.Height / 2 - (point.Y / point.Z) * focalLength);
+				Vector2 p1 = ProjectPoints(triangle.Item1);
 
 				e.Graphics.DrawRectangle(new Pen(Color.Red), ScreenX, ScreenY, 2, 2);
 			}
