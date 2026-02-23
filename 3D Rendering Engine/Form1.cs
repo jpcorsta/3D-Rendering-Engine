3a2db0455b7e018ac330f3143bb12e61fc8ff3e5
jean-paul jeursen <jpcorsta@mail.com>
2026-02-23 08:26:04 +0100

    https://www.youtube.com/watch?v=oHP3QFvNpn4&list=PLaoUW4S6Hk__bLDdXkDNOW7yJ3jQkrdWS&t=17m55s

--- 3D Rendering Engine/Form1.cs
+++ 3D Rendering Engine/Form1.cs
@@ -6,7 +6,7 @@ namespace _3D_Rendering_Engine
 	{
 		float focalLength = 200f;
 
-		List<(Vector3, Vector3, Vector3)> points = new List<(Vector3, Vector3, Vector3)>();
+		List<(Vector3, Vector3, Vector3)> Triangles = new List<(Vector3, Vector3, Vector3)>();
 
 		Vector2 ProjectPoints(Vector3 point)
 		{
@@ -19,29 +19,26 @@ namespace _3D_Rendering_Engine
 		public Form1()
 		{
 			InitializeComponent();
-
-			// front face
-			points.Add(new Vector3(-1, -1, 2));
-			points.Add(new Vector3(1, -1, 2));
-			points.Add(new Vector3(-1, 1, 2));
-			points.Add(new Vector3(1, 1, 2));
-
-			// front face
-			points.Add(new Vector3(-1, -1, 4));
-			points.Add(new Vector3(1, -1, 4));
-			points.Add(new Vector3(-1, 1, 4));
-			points.Add(new Vector3(1, 1, 4));
-
-
+			
+			Triangles.Add(new Vector3(1, 1, 1), new Vector3(1, 1, 1) new Vector3(1, 1, 1))
 		}
 
 		private void Form1_Paint(object sender, PaintEventArgs e)
 		{
-			foreach (var triangle in points) 
+			foreach (var triangle in Triangles) 
 			{
 				Vector2 p1 = ProjectPoints(triangle.Item1);
+				Vector2 p2 = ProjectPoints(triangle.Item2);
+				Vector2 p3 = ProjectPoints(triangle.Item3);
 
-				e.Graphics.DrawRectangle(new Pen(Color.Red), ScreenX, ScreenY, 2, 2);
+				PointF[] TrianglePoints = new PointF[3]
+				{
+					new PointF(p1.X, p1.Y),
+					new PointF(p2.X, p2.Y),
+					new PointF(p3.X, p3.Y),
+				};
+
+				e.Graphics.DrawPolygon(new Pen(Color.Red), TrianglePoints);
 			}
 		}
 	}
